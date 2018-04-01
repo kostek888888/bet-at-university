ebc4b72c136624319d77796f078cf38b19e44ebe
Michał Kostecki <kostek888888@gmail.com>
2018-04-02 00:18:15 +0200

    add example

---
+++ src/main/java/Example.java
@@ -0,0 +1,21 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+    @RestController
+    @EnableAutoConfiguration
+    public class Example {
+
+        @RequestMapping("/")
+        String home() {
+            return "Spring test";
+        }
+
+        public static void main(String[] args) throws Exception {
+            SpringApplication.run(Example.class, args);
+        }
+
+    }
+
