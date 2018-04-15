2f8f3b114eb5e39d35485aed58484d9757e80799
Michał Kostecki <kostek888888@gmail.com>
2018-04-15 21:15:12 +0200

    gradle build to war
    Signed-off-by: Michał Kostecki <kostek888888@gmail.com>

--- src/main/java/Example.java
+++ src/main/java/Example.java
@@ -1,16 +1,23 @@
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
     @RestController
     @EnableAutoConfiguration
-    public class Example {
+    public class Example extends SpringBootServletInitializer {
+
+        @Override
+        protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+            return application.sources(Example.class);
+        }
 
         @RequestMapping("/")
         String home() {
-            return "Spring test";
+            return "Spring test with war";
         }
 
         public static void main(String[] args) throws Exception {
