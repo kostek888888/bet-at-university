455bb060889221e00d7c05f3c3e9d4073e50f96e
wojtek <wojtek.klusek2@gmail.com>
2018-05-20 18:00:45 +0100

    add MatchesResult page v0.1
    @TODO
    -add data model,
    -add map teamId to teamName (you must add
    teamId in api, to search insted team name),
    -display data in table on page

--- Angular Client/angular-client/src/app/games-results/games-results.component.ts
+++ Angular Client/angular-client/src/app/games-results/games-results.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpService } from '../http.service';
 
 @Component({
   selector: 'app-games-results',
@@ -7,9 +8,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GamesResultsComponent implements OnInit {
 
-  constructor() { }
+  gamesResults = null;
+  formObj = new FormModel();
+  constructor(private httpService: HttpService) { }
 
   ngOnInit() {
   }
 
+  getSearchResult() {
+    if (this.formObj.filter === 'searchMatchday') {
+      this.httpService.getFixturesByMatchday(this.formObj.searchText).subscribe(response => {
+        this.gamesResults = JSON.stringify(response);
+        console.log(response);
+      });
+    } else {
+      // pobierz mecze dla druzyny 3 dni przed i 3 dni wprzod
+      this.httpService.getPastFixturesByTeam(this.formObj.searchText, '3').subscribe(response => {
+        this.gamesResults = JSON.stringify(response);
+        console.log(response);
+      });
+
+      this.httpService.getNextFixturesByTeam(this.formObj.searchText, '3').subscribe(response => {
+        this.gamesResults += JSON.stringify(response);
+        console.log(response);
+      });
+    }
+  }
+
+}
+
+
+export class FormModel {
+  searchText?: string;
+  filter = 'searchMatchday';
 }
--- Angular Client/angular-client/src/app/http.service.ts
+++ Angular Client/angular-client/src/app/http.service.ts
@@ -3,14 +3,39 @@ import { Observable } from 'rxjs/Observable';
 import { HttpParams, HttpClient, HttpHeaders } from '@angular/common/http';
 import { LeagueTableModel } from './models/leagueTableModel';
 
+
+const competitionId = 445; // id dla ligi angielskiej
+
 @Injectable()
 export class HttpService {
 
   apiKeyHeader = new HttpHeaders().set('X-Auth-Token', '5407750046da4af5a387714fba7ea516');
-
   constructor(private http: HttpClient) { }
 
   getLeagueTable(): Observable<LeagueTableModel> {
-    return this.http.get<LeagueTableModel>('http://api.football-data.org/v1/competitions/445/leagueTable', {headers: this.apiKeyHeader});
+    return this.http.get<LeagueTableModel>('http://api.football-data.org/v1/competitions/' +
+      competitionId + '/leagueTable', { headers: this.apiKeyHeader });
+  }
+
+  getFixtures() {
+    return this.http.get('http://api.football-data.org/v1/competitions/' +
+      competitionId + '/fixtures', { headers: this.apiKeyHeader });
+  }
+
+  getFixturesByMatchday(matchday: string) {
+    return this.http.get('http://api.football-data.org/v1/competitions/' +
+      competitionId + '/fixtures?matchday=' + matchday, { headers: this.apiKeyHeader });
+  }
+
+  // pobierz mecze druzyny (teamid) na nastepne dni (days) domyslnie 7
+  getNextFixturesByTeam(teamId: string, nextDays: string = '7') {
+    return this.http.get('http://api.football-data.org/v1/teams/' + teamId + '/fixtures?timeFrame=n' +
+     nextDays, { headers: this.apiKeyHeader });
+  }
+
+  // to co wyzej tylko poprzednie dni
+  getPastFixturesByTeam(teamId: string, pastDays: string = '7') {
+    return this.http.get('http://api.football-data.org/v1/teams/' + teamId + '/fixtures?timeFrame=p' +
+      pastDays, { headers: this.apiKeyHeader });
   }
 }
