54a25b01da802b076bc62ef628a5226094f6091a
wojtek <wojtek.klusek2@gmail.com>
2018-05-24 22:38:09 +0100

    add dev and prod config
    + add print first 5 games scores in games scores page, and sort this
    scores by date

--- Angular Client/angular-client/src/app/http.service.ts
+++ Angular Client/angular-client/src/app/http.service.ts
@@ -3,19 +3,22 @@ import { Observable } from 'rxjs/Observable';
 import { HttpParams, HttpClient, HttpHeaders } from '@angular/common/http';
 import { LeagueTableModel } from './models/leagueTableModel';
 import { MatchModel } from './models/matchModel';
+import { environment } from '../environments/environment';
 
 
 
 @Injectable()
 export class HttpService {
-
+  private server_link = environment.config.server_link;
   constructor(private http: HttpClient) { }
 
   getLeagueTable(): Observable<LeagueTableModel> {
-    return this.http.get<LeagueTableModel>('http://localhost:8080/team');
+    console.log(this.server_link + 'team');
+    return this.http.get<LeagueTableModel>(this.server_link + 'team');
   }
 
   getMatch(): Observable<MatchModel> {
-    return this.http.get<MatchModel>('http://localhost:8080/match');
+    console.log(this.server_link + 'match');
+    return this.http.get<MatchModel>(this.server_link + 'match');
   }
 }
