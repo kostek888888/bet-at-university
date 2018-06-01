72d67728bf669dd36d69d7b9d2330acafa3aba5a
tumbumann <pawel14157@gmail.com>
2018-06-01 22:46:13 +0200

    add bet, auto check bet result and save user statistics after match

--- src/main/java/bet_at_university/controllers/UserStatisticsController.java
+++ src/main/java/bet_at_university/controllers/UserStatisticsController.java
@@ -16,22 +16,6 @@ public class UserStatisticsController {
     @Autowired
     private UserStatisticRepository userStatisticRepository;
 
-    @CrossOrigin(origins="http://localhost:4200")
-    @PostMapping(path = "/addUS")
-    public @ResponseBody void addUserStatistics(@RequestParam int accountBalance, double biggestWin,
-                                                String biggestWinDate, int wonOrLost){
-        UserStatistics userStatistics = new UserStatistics();
-        userStatistics.setAccountBalance(accountBalance);
-        userStatistics.setBiggestWin(biggestWin);
-        userStatistics.setBiggestWinDate(biggestWinDate);
-        if(wonOrLost == 0){
-            userStatistics.setWonMatches(userStatistics.getWonMatches()+1);
-        }
-        else {
-            userStatistics.setLostMatches(userStatistics.getLostMatches()+1);
-        }
-        userStatisticRepository.save(userStatistics);
-    }
 
     @CrossOrigin(origins="http://localhost:4200")
     @GetMapping(path = "/allUsersStatistics")
