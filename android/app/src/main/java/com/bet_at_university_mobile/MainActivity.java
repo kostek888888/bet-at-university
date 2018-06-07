82b118ea8299a9848bbec04ada9d8960edfc1cca
tumbumann <pawel14157@gmail.com>
2018-06-07 22:05:06 +0200

    fix android web view, fix bet date and chenge site title

--- android/app/src/main/java/com/bet_at_university_mobile/MainActivity.java
+++ android/app/src/main/java/com/bet_at_university_mobile/MainActivity.java
@@ -3,6 +3,7 @@ package com.bet_at_university_mobile;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
     private WebView webView;
@@ -15,9 +16,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
      webView = (WebView) findViewById(R.id.webView);
-     webView.loadUrl("http://tu.kielce.pl");
+     webView.loadUrl("http://10.0.2.2:4200/");
+//     webView.loadUrl("http://betatuniversity.ddns.net/angular2");
 
      webView.getSettings().setJavaScriptEnabled(true);
      webView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
+     webView.setWebViewClient(new WebViewClient());
     }
 }
